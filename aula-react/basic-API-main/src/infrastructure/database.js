0538c93e5061d3106ebd73dde3b809381c841786
Mariana <87673848+MaMBassi@users.noreply.github.com>
2022-11-12 20:44:21 -0300

    basic-API
    Usuarios

---
+++ aula-react/basic-API-main/src/infrastructure/database.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const uri = `mongodb+srv://user:<password>@cluster0.u4vn0hv.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    id: {
+        type: String,
+        index: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+    },
+    nome: String,
+    senha: String,
+});
+
+const UserModel = mongoose.model('UserModel', UserSchema);
+
+module.exports = {
+    UserModel,
+};
